e4e51fd47f1789ad905265ad261263db78c40424
funan <1034181928@qq.com>
2021-02-11 21:48:12 +0800

    2.11

--- 正则表达式/main.js
+++ 正则表达式/main.js
@@ -12,3 +12,20 @@ function replace(e) {
 }
 
 document.write(replace(document.getElementById('replace').innerHTML))
+
+// 数字格式化
+function formatNum(num) {
+  var left = num.split('.')[0]
+  var right = num.split('.')[1]
+  var reg1 = /\B(?=(\d{3})+$)/g
+  var reg2 = /(?<=^(\d{3})+)\B/g
+  return `${left.replace(reg1, ',')}.${right.replace(reg2, ',')}`
+}
+
+function test(e) {
+  var reg = /c/
+  return e.search(reg)
+}
+
+console.log(formatNum('122342634568.8712246345'))
+console.log(test('abcdef'))
